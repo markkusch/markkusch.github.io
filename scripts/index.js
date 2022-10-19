e801cba4bfcbbc393e3f93c44333a5fe05752a4c
Markus <alu0101130507@ull.edu.es>
2022-10-19 16:19:34 +0100

    canvas resizing added

--- scripts/index.js
+++ scripts/index.js
@@ -13,14 +13,14 @@ class Star {
   constructor() {
     this.x = random(-width, width);
     this.y = random(-height, height);
-    this.z = random(width);
+    this.z = random(1920);
     this.color = starsColors[floor(random(0, starsColors.length))];
   }
 
   update() {
     this.z = this.z - 10;
     if (this.z < 1) {
-      this.z = width;
+      this.z = 1920;
       this.x = random(-width, width);
       this.y = random(-height, height);
     }
@@ -31,7 +31,7 @@ class Star {
     noStroke();
     let sx = map(this.x / this.z, 0, 1, 0, width);
     let sy = map(this.y / this.z, 0, 1, 0, height);
-    let r = map(this.z, 0, width, 16, 0);
+    let r = map(this.z, 0, 1920, 16, 0);
     ellipse(sx, sy, r, r);
   }
 }
@@ -51,4 +51,8 @@ function draw() {
     stars[i].update();
     stars[i].show();
   }
+}
+
+function windowResized() {
+  resizeCanvas(width = document.body.clientWidth, height = document.body.clientHeight);
 }
