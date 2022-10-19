967dae566e5347efda961b2c5366f55f9de6469e
Markus <alu0101130507@ull.edu.es>
2022-10-19 13:20:29 +0100

    starfield in p5.js added

---
+++ scripts/index.js
@@ -0,0 +1,55 @@
+let stars = [];
+let starsColors = [
+  '#afc9ff',
+  '#c7d8ff',
+  '#fff4f3',
+  '#ffe5cf',
+  '#ffd9b2',
+  '#ffc78e',
+  '#ffa651'
+]
+
+class Star {
+  constructor() {
+    this.x = random(-width, width);
+    this.y = random(-height, height);
+    this.z = random(width);
+    this.color = starsColors[floor(random(0, starsColors.length))];
+  }
+
+  update() {
+    this.z = this.z - 10;
+    if (this.z < 1) {
+      this.z = width;
+      this.x = random(-width, width);
+      this.y = random(-height, height);
+    }
+  }
+
+  show() {
+    fill(this.color);
+    noStroke();
+    let sx = map(this.x / this.z, 0, 1, 0, width);
+    let sy = map(this.y / this.z, 0, 1, 0, height);
+    let r = map(this.z, 0, width, 16, 0);
+    ellipse(sx, sy, r, r);
+  }
+}
+
+function setup() {
+  let cnv = createCanvas(width = document.body.clientWidth, height = document.body.clientHeight);
+  cnv.parent('content'); // Maybe change it to 'content' if you want to apply it to the whole website
+  for (let i = 0; i < 1000; i++) {
+    stars.push(new Star());
+  }
+}
+
+function draw() {
+  clear();
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].update();
+    stars[i].show();
+  }
+}
